abbb26c9f60ed4c7c51cf26333f1c04699cc3a7c
coral <jonas@jonasbengtson.se>
2023-04-29 11:13:44 -0700

    test

--- src/ddp.rs
+++ src/ddp.rs
@@ -2,7 +2,7 @@
 #[derive(Debug)]
 #[repr(u8)]
 #[allow(dead_code)]
-enum DataType {
+pub enum DataType {
     UndefinedType,
     RGB,
     HSL,
@@ -10,11 +10,10 @@ enum DataType {
     Grayscale,
 }
 
-
 #[derive(Debug)]
 #[repr(u8)]
 #[allow(dead_code)]
-enum PixelFormat {
+pub enum PixelFormat {
     UndefinedPixelFormat,
     Pixel1Bits,
     Pixel4Bits,
@@ -26,25 +25,129 @@ enum PixelFormat {
 
 #[derive(Debug)]
 #[allow(dead_code)]
-struct PixelConfig {
-    data_type: DataType,
-    data_size: PixelFormat,
-    customer_defined: bool,
+pub struct PixelConfig {
+    pub data_type: DataType,
+    pub data_size: PixelFormat,
+    pub customer_defined: bool,
 }
 
-#[derive(Debug)]
-struct PacketType {
-    timecode: bool,
-    storage: bool,
-    reply: bool,
-    query: bool,
-    push: bool,
+
+#[derive(Debug, PartialEq)]
+#[allow(dead_code)]
+pub struct PacketType {
+    version: u8, // 0x40 ( 2 bit value, can be a value between 1-4 depending on version mask, )
+    pub timecode: bool, // 0x10
+    pub storage: bool, // 0x08
+    pub reply: bool, // 0x04
+    pub query: bool, //0x02
+    pub push: bool, //0x01
 }
+
+const VERSION_MASK: u8 = 0xc0;
+//const RESERVED: u8 = 0x20;
+const TIMECODE: u8 = 0x10;
+const STORAGE: u8 = 0x08;
+const REPLY: u8 = 0x04;
+const QUERY: u8 = 0x02;
+const PUSH: u8 = 0x01;
+
+impl PacketType {}
+
+impl From<u8> for PacketType {
+    fn from(byte: u8) -> Self {
+
+        let version = match byte & VERSION_MASK {
+            0x00 => 1,
+            0x40 => 2,
+            0x80 => 3,
+            0xc0 => 4,
+            _ => 0,
+        };
+        let timecode = byte & TIMECODE == TIMECODE;
+        let storage = byte & STORAGE == STORAGE;
+        let reply = byte & REPLY == REPLY;
+        let query = byte & QUERY == QUERY;
+        let push = byte & PUSH == PUSH;
+
+        PacketType {
+            version,
+            timecode,
+            storage,
+            reply,
+            query,
+            push,
+        }
+    }
+}
+
+impl Into<u8> for PacketType {
+    fn into(self) -> u8 {
+        let mut byte: u8 = 0;
+        let v = match self.version {
+            1 => self.version,
+            2 => self.version,
+            3 => self.version,
+            4 => self.version,
+            _ => 0,
+        };
+        byte |= v << 6;
+        // Set the flag bits
+        if self.timecode { byte |= TIMECODE };
+        if self.storage { byte |= STORAGE };
+        if self.reply { byte |= REPLY };
+        if self.query { byte |= QUERY };
+        if self.push { byte |= PUSH };
+
+        byte
+    }
+}
+
+
 
 pub struct Header {
-    sequence_number: u8,
-    data_type: PixelConfig,
-    id: u8,
-    offset: u32,
-    length: u16,
+    pub packet_type: PacketType,
+    pub sequence_number: u8,
+    pub pixel_config: PixelConfig,
+    pub id: u8,
+    pub offset: u32,
+    pub length: u16,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_packet_type_parse() {
+        let byte: u8 = 0b00010110;
+        let packet_type = PacketType::from(byte);
+
+        assert_eq!(
+            packet_type,
+            PacketType {
+                version: 1,
+                timecode: true,
+                storage: false,
+                reply: true,
+                query: true,
+                push: false,
+            }
+        );
+    }
+
+
+    #[test]
+    fn test_packet_type_into_u8() {
+        let packet_type = PacketType {
+            version: 1,
+            timecode: true,
+            storage: false,
+            reply: true,
+            query: true,
+            push: false,
+        };
+
+        let byte: u8 = packet_type.into();
+        assert_eq!(byte, 0x56);
+    }
 }
