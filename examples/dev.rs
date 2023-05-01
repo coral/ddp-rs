b6470be0c33912b344ebdb4b8c54a3fa83e084f5
coral <jonas@jonasbengtson.se>
2023-04-30 20:32:59 -0700

    prd

--- examples/dev.rs
+++ examples/dev.rs
@@ -12,7 +12,7 @@ fn main() {
         )
         .unwrap();
 
-    c.write(&vec![255, 255, 255, 128, 128, 128], 0).unwrap();
+    c.write(&vec![255, 255, 255, 128, 128, 12], 0).unwrap();
 
     let ten_millis = time::Duration::from_secs(10);
     let now = time::Instant::now();
